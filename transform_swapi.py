1d13f988571ba20450dcddae601748f8b122f5bc
Rob Griffin <robgriffin247@hotmail.com>
2026-04-14 09:52:29 +0200

    code to transform data from raw to multiple assets along the pipeline until a desired film_stats table

---
+++ transform_swapi.py
@@ -0,0 +1,80 @@
+from spark_session import get_spark_session
+import pyspark.sql.functions as sf
+
+
+def build_characters(spark_session):
+
+    characters = spark_session.read.parquet("data/raw/people.parquet")
+
+    # Fix numeric type in height and mass
+    characters = characters.withColumn("height", sf.regexp_replace("height", ",", ""))
+    characters = characters.withColumn("mass", sf.regexp_replace("mass", ",", ""))
+
+    # > Columns to retain from characters
+    characters = characters.select(
+        sf.col("url").alias("character_url"),
+        sf.col("name").alias("character"),
+        sf.col("height").try_cast("integer"),
+        sf.col("mass").try_cast("float"),
+    )
+
+    characters.write.format("delta").mode("overwrite").save("data/delta/characters")
+    return characters
+
+
+def build_film_characters(spark_session):
+
+    films = spark_session.read.parquet("data/raw/films.parquet")
+    characters = spark_session.read.format("delta").load("data/delta/characters")
+
+    # Reshape data
+    # > Expand films character array into one row per character per film
+    film_characters = films.select(
+        sf.col("url").alias("film_url"),
+        sf.col("episode_id").alias("episode"),
+        sf.col("title").alias("film"),
+        sf.explode("characters").alias("character_url"),
+    )
+
+    # > Join
+    film_characters = film_characters.join(characters, "character_url")
+
+    film_characters.write.format("delta").mode("overwrite").save(
+        "data/delta/film_characters"
+    )
+    return film_characters
+
+
+def build_film_stats(spark_session):
+
+    film_characters = spark_session.read.format("delta").load(
+        "data/delta/film_characters"
+    )
+
+    # Aggregate to films stats; number of characters and average height/mass per film
+    film_stats = (
+        film_characters.groupBy(["film_url", "film", "episode"])
+        .agg(
+            sf.count("character_url").alias("character_count"),
+            sf.round(sf.avg("mass"), 1).alias("average_mass"),
+            sf.round(sf.avg("height"), 1).alias("average_height"),
+        )
+        .sort("film_url")
+        .drop("film_url")
+    )
+
+    film_stats.write.format("delta").mode("overwrite").save("data/delta/film_stats")
+    return film_stats
+
+
+if __name__ == "__main__":
+
+    spark_session = get_spark_session()
+
+    build_characters(spark_session)
+    build_film_characters(spark_session)
+    build_film_stats(spark_session)
+
+    spark_session.read.format("delta").load("data/delta/characters").show()
+    spark_session.read.format("delta").load("data/delta/film_characters").show()
+    spark_session.read.format("delta").load("data/delta/film_stats").show()
